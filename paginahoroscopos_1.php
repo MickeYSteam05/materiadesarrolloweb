9f5e5634ffd71b440d1376a57f6aab461fabefac
leorojasc <leorojasas@gmail.com>
2026-02-19 23:54:25 -0600

    Update paginahoroscopos_1.php

--- paginahoroscopos_1.php
+++ paginahoroscopos_1.php
@@ -1,16 +1,25 @@
+<?php 
+    $genero = $_REQUEST['genero'] ?? '';
+
+    $clase_estilo = ($genero == "mujer") ? "calido" : "frio";
+
+    $nombre = $_REQUEST['nombre'] ?? '';
+    $ap_paterno = $_REQUEST['ap_paterno'] ?? '';
+    $ap_materno = $_REQUEST['ap_materno'] ?? '';
+    $nick = $_REQUEST['nick'] ?? '';
+    $correo = $_REQUEST['correo'] ?? '';
+    $contrasena = $_REQUEST['contrasena'] ?? '';
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
   <meta charset="UTF-8">
-  <title>TITULO</title>
+  <title>Datos del Formulario</title>
   <link rel="stylesheet" href="style1.css">
 </head>
 
-
-
-<body>
+<body class="<?php echo $clase_estilo; ?>">
   <header class="header">
-
     <nav class="menu" id="menu">
         <a href="index.html"> Pagina principal</a>
         <a href="paginamickey.html"> Mickey</a>
@@ -18,19 +27,12 @@
         <a href="paginaleo.html"> Leo </a>
         <a href="paginaemilio.html"> Emilio </a>
     </nav>
-</header>
+  </header>
+
   <h1>Datos del formulario</h1>
 
-  <p>
+  <p class="datos-resultado">
     <?php 
-        $nombre = $_REQUEST['nombre'];
-        $ap_paterno = $_REQUEST['ap_paterno'];
-        $ap_materno = $_REQUEST['ap_materno'];
-        $nick = $_REQUEST['nick'];
-        $correo = $_REQUEST['correo'];
-        $contrasena = $_REQUEST['contrasena'];
-        $genero = $_REQUEST['genero'];
-
         echo ("El nombre es $nombre<br>");
         echo ("El apellido paterno es $ap_paterno<br>");
         echo ("El apellido materno es $ap_materno<br>");
@@ -41,14 +43,9 @@
     ?>
   </p>
 
-
   <footer class="footer">
     <p class="no-padding">© 2026. All rights reserved to the students.</p>
   </footer>
   <script src="script.js"></script>
 </body>
 </html>
-
-
-
-
